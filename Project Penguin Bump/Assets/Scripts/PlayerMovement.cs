c6b82a052a4d5834500f4ded663bc28da8a13d7b
Leila <leila.hallenberg@starstable.com>
2020-01-31 22:38:19 +0100

    Add bounce to penguins

--- Project Penguin Bump/Assets/Scripts/PlayerMovement.cs
+++ Project Penguin Bump/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public string HorizontalMove;
     public string VerticalMove;
+    public float force;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         if (speed == 0) { speed = 1; }
+        if (force == 0) { force = 100; }
     }
 
     // Update is called once per frame
@@ -23,7 +25,25 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis(VerticalMove);
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+        
         rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter(Collision col) 
+    { 
+        if (col.gameObject.tag == "Player") 
+        {
+            Debug.Log("COLLIDING");
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = col.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir * force);
+
+            //rb.velocity = col.gameObject.GetComponent<Rigidbody>().velocity;
+            //rb.AddForce(Vector3 * -speed);
+        }
     }
 }
