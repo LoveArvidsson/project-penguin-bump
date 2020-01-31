8d2ad509a34223e60bf8b5705441431fbf8b6319
Leila <leila.hallenberg@starstable.com>
2020-01-31 21:02:18 +0100

    Add basic movement

--- Project Penguin Bump/Assets/PlayerMovement.cs
+++ Project Penguin Bump/Assets/PlayerMovement.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        speed = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
+            this.GetComponent<Rigidbody>().addForce(1);
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -26,5 +29,12 @@ public class PlayerMovement : MonoBehaviour
             position.x++;
             this.transform.position = position;
         }
+        */
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
