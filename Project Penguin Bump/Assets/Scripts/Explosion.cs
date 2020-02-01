bf3e890dd14569e47b4da4b40fb04fd64b4fd797
Leila <leila.hallenberg@starstable.com>
2020-02-01 14:26:35 +0100

    Update bombs to work properly

--- Project Penguin Bump/Assets/Scripts/Explosion.cs
+++ Project Penguin Bump/Assets/Scripts/Explosion.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     public float Force;
+    public float timer;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (timer == 0) { timer = 0.3f; }
         if (Force == 0) { Force = 300;  }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,7 +32,7 @@ public class Explosion : MonoBehaviour
             Debug.Log("Colliding explosion and player");
             Vector3 dir = other.transform.position - transform.position;
             dir.Normalize();
-            other.gameObject.GetComponent<Rigidbody>().AddForce(dir);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(dir * Force);
         }
     }
 
