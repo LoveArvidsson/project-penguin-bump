f07042ea5e4b83368e428eaa5dfbde6d11659761
Gibbz <gabriel.constantinou@starstable.com>
2020-02-02 16:13:40 +0100

    commit cause leila told me to

---
+++ Project Penguin Bump/Assets/Scripts/AutoSkip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoSkip : MonoBehaviour
+{
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.Deltatime;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
